0df07a91598823aa28400ad83b52ee4f74d9e00d
gmbrakebill <64878965+gmbrakebill@users.noreply.github.com>
2020-06-26 14:54:30 -0500

    [update]

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,115 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.send("It works")
 })
+app.get('/users', (req, res) => {
+    queries.getUsers(req,res);
+})
+app.post('/users', (req,res) =>{
+    queries.createNewUser(req,res);
+})
+app.get('/manufacturers', (req, res) => {
+    queries.getManufacturers(req,res);
+})
+app.post('/manufacturers', (req,res) =>{
+    queries.createNewManufacturer(req,res);
+})
+app.get('/customers', (req, res) => {
+    queries.getCustomers(req,res);
+})
+app.post('/customers', (req,res) =>{
+    queries.createNewCustomer(req,res);
+})
+app.get('/items', (req, res) => {
+    queries.getItems(req,res);
+})
+app.post('/items', (req,res) =>{
+    queries.createNewItems(req,res);
+})
+app.get('/purchase-orders', (req, res) => {
+    queries.getPurchaseOrders(req,res);
+})
+app.post('/purchase-orders', (req,res) =>{
+    queries.createNewPurchaseOrder(req,res);
+})
+app.get('/sale-orders', (req, res) => {
+    queries.getSalesOrders(req,res);
+})
+app.post('/sale-orders', (req,res) =>{
+    queries.createNewSaleOrder(req,res);
+})
+app.get('/user/:id', (req, res) => {
+    queries.getUsersById(req,res);
+})
+app.delete('/user/:id', (req, res)=>
+{
+    queries.deleteUser(req,res);
+})
+app.put('/user/:id', (req, res)=>
+{
+    queries.updateUser(req,res);
+})
+
+app.get('/manufacturer/:id', (req, res) => {
+    queries.getManufacturerById(req,res);
+})
+app.delete('/manufacturer/:id', (req, res)=>
+{
+    queries.deleteManufacturer(req,res);
+})
+app.put('/manufacturer/:id', (req, res)=>
+{
+    queries.updateManufacturer(req,res);
+})
+
+app.get('/customer/:id', (req, res) => {
+    queries.getCustomerById(req,res);
+})
+app.delete('/customer/:id', (req, res)=>
+{
+    queries.deleteCustomers(req,res);
+})
+app.put('/customer/:id', (req, res)=>
+{
+    queries.updateCustomer(req,res);
+})
+
+app.get('/sale-order/:id', (req, res) => {
+    queries.getSaleOrdersById(req,res);
+})
+app.delete('/sale-order/:id', (req, res)=>
+{
+    queries.deleteSaleOrders(req,res);
+})
+app.put('/sale-order/:id', (req, res)=>
+{
+    queries.updateSaleOrders(req,res);
+})
+
+app.get('/purchase-order/:id', (req, res) => {
+    queries.getPurchaseOrderById(req,res);
+})
+app.delete('/purchase-order/:id', (req, res)=>
+{
+    queries.deletePurchasedOrders(req,res);
+})
+app.put('/purchase-order/:id', (req, res)=>
+{
+    queries.updatePurchaseOrders(req,res);
+})
+
+app.get('/item/:id', (req,res) =>{
+    queries.getItemById(req,res);
+})
+app.delete('/item/:id', (req, res)=>
+{
+    queries.deleteItems(req,res);
+})
+app.put('/item/:id', (req, res)=>
+{
+    queries.updateItems(req,res);
+})
+
+
 
 const port = 3000
 app.listen(port, () => console.log('App listening'))
--- backend/queries.js
+++ backend/queries.js
@@ -7,17 +7,10 @@ const pool = new Pool({
     port: 5432,
 })
 
-pool.query("SELECT * FROM table_name", (error, results) => {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log(results.rows)
-    }
-})
 
 function getUsers(req,res)
 {
-    pool.query("SELECT * FROM users_table", (error, results) => {
+    pool.query("SELECT * FROM users", (error, results) => {
         if (error) {
             console.log(error)
         } else {
@@ -25,5 +18,408 @@ function getUsers(req,res)
         }
     })
 }
+function getManufacturers(req,res)
+{
+    pool.query("SELECT * FROM manufacturers", (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.send(results.rows)
+        }
+    })
+}
+function getCustomers(req,res)
+{
+    pool.query("SELECT * FROM customers", (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.send(results.rows)
+        }
+    })
+}
+function getItems(req,res)
+{
+    pool.query("SELECT * FROM items", (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.send(results.rows)
+        }
+    })
+}
+function getPurchaseOrders(req,res)
+{
+    pool.query("SELECT * FROM purchased_orders", (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            for(const row of results.rows)
+            {
+                row.users_id = `http://${req.headers.host}/user/${row.users_id}`;
+                row.manufacturers_id = `http://${req.headers.host}/manufacturer/${row.manufacturers_id}`
+            }
+            res.send(results.rows)
+        }
+    })
+}
+
+function getSalesOrders(req,res)
+{
+    pool.query("SELECT * FROM sale_orders ", (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            for(const row of results.rows)
+            {
+                row.users_id = `http://${req.headers.host}/user/${row.users_id}`;
+                row.customers_id = `http://${req.headers.host}/customer/${row.customers_id}`
+            }
+            res.send(results.rows)
+        
+        }
+    })
+}
+function getManufacturerById(req,res)
+{
+    pool.query("SELECT * FROM manufacturers WHERE id = $1 ", [req.params.id], (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.send(results.rows)
+            
+        }
+    })
+}
+function getItemById(req,res)
+{
+    pool.query("SELECT * FROM items WHERE id = $1 ", [req.params.id], (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.send(results.rows)
+            
+        }
+    })
+}
+function getUsersById(req,res)
+{
+    pool.query("SELECT * FROM users WHERE id = $1 ", [req.params.id], (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.send(results.rows)
+        }
+    })
+}
+function getCustomerById(req,res)
+{
+    pool.query("SELECT * FROM customers WHERE id = $1 ", [req.params.id], (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.send(results.rows)
+        }
+    })
+}
+function getPurchaseOrderById(req,res)
+{
+    pool.query("SELECT * FROM purchased_orders WHERE id = $1 ", [req.params.id], (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            for(const row of results.rows)
+            {
+                row.users_id = `http://${req.headers.host}/user/${row.users_id}`;
+                row.manufacturers_id = `http://${req.headers.host}/manufacturer/${row.manufacturers_id}`
+            }
+            res.send(results.rows)
+        
+        }
+    })
+}
+function getSaleOrdersById(req,res)
+{
+    pool.query("SELECT * FROM sale_orders WHERE id = $1 ", [req.params.id], (error, results) => {
+        if (error) {
+            console.log(error)
+        } else {
+            for(const row of results.rows)
+            {
+                row.users_id = `http://${req.headers.host}/user/${row.users_id}`;
+                row.customers_id = `http://${req.headers.host}/customer/${row.customers_id}`
+            }
+            res.send(results.rows)
+        
+        }
+    })
+}
+function createNewUser(req,res)
+{
+    pool.query("INSERT INTO users (first_name, last_name, email) VALUES ($1,$2,$3) ", [req.body.firstname, req.body.lastname, req.body.email], (error,results) =>{
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(201).send();
+        }
+
+    })
+}
+function createNewManufacturer(req,res)
+{
+    pool.query("INSERT INTO manufacturers (company_name, contact_name, contact_email, contact_phone) VALUES ($1,$2,$3, $4) ", [req.body.companyname, req.body.contactname, req.body.contactemail, req.body.contactphone], (error,results) =>{
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(201).send();
+        }
+
+    })
+}
+function createNewCustomer(req,res)
+{
+    pool.query("INSERT INTO customers (company_name, contact_name, contact_email, contact_phone) VALUES ($1,$2,$3, $4) ", [req.body.companyname, req.body.contactname, req.body.contactemail, req.body.contactphone], (error,results) =>{
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(201).send();
+        }
+
+    })
+}
+function createNewItems(req,res)
+{
+    pool.query("INSERT INTO items (names, descriptions) VALUES ($1,$2) ", [req.body.names, req.body.descriptions], (error,results) =>{
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(201).send();
+        }
+
+    })
+}
+function createNewPurchaseOrder(req,res)
+{
+    pool.query("INSERT INTO purchased_orders (users_id, manufacturers_id, date_ordered, date_received) VALUES ($1,$2,$3, $4) ", [req.body.userId, req.body.manufacturers_id, req.body.dateOrdered, req.body.dateReceived], (error,results) =>{
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(201).send();
+        }
+
+    })
+}
+function createNewSaleOrder(req,res)
+{
+    pool.query("INSERT INTO sale_orders (users_id, customers_id, date_ordered, date_received) VALUES ($1,$2,$3, $4) ", [req.body.userId, req.body.customersId, req.body.dateOrdered, req.body.dateReceived], (error,results) =>{
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(201).send();
+        }
+
+    })
+}
+function deleteUser(req,res)
+{
+    pool.query("DELETE FROM users WHERE id = $1", [req.params.id], (error, results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(204).send();
+        }
+    })
+}
+function deleteManufacturer(req,res)
+{
+    pool.query("DELETE FROM manufacturers WHERE id = $1", [req.params.id], (error, results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(204).send();
+        }
+    })
+}
+function deleteCustomers(req,res)
+{
+    pool.query("DELETE FROM customers WHERE id = $1", [req.params.id], (error, results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(204).send();
+        }
+    })
+}
+function deleteItems(req,res)
+{
+    pool.query("DELETE FROM items WHERE id = $1", [req.params.id], (error, results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(204).send();
+        }
+    })
+}
+function deletePurchasedOrders(req,res)
+{
+    pool.query("DELETE FROM purchased_orders WHERE id = $1", [req.params.id], (error, results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(204).send();
+        }
+    })
+}
+function deleteSaleOrders(req,res)
+{
+    pool.query("DELETE FROM sale_orders WHERE id = $1", [req.params.id], (error, results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(204).send();
+        }
+    })
+}
+
+function updateUser(req,res)
+{
+    pool.query("UPDATE users SET first_name = $1, last_name = $2, email = $3 WHERE id = $4",[req.body.firstname, req.body.lastname,req.body.email, req.params.id], (error,results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(202).send();
+        }
+    }) 
+}
+function updateManufacturer(req,res)
+{
+    pool.query("UPDATE manufacturers SET company_name = $1, contact_name = $2, contact_email = $3, contact_phone = $4 WHERE id = $5",[req.body.companyname, req.body.contactname,req.body.contactemail, req.body.contactphone, req.params.id], (error,results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(202).send();
+        }
+    }) 
+}
+function updateCustomer(req,res)
+{
+    pool.query("UPDATE customers SET company_name = $1, contact_name = $2, contact_email = $3, contact_phone = $4 WHERE id = $5",[req.body.companyname, req.body.contactname,req.body.contactemail, req.body.contactphone, req.params.id], (error,results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(202).send();
+        }
+    }) 
+}
+function updateItems(req,res)
+{
+    pool.query("UPDATE items SET names = $1, descriptions = $2 WHERE id = $3",[req.body.companyname, req.body.contactname, req.params.id], (error,results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(202).send();
+        }
+    }) 
+}
+function updatePurchaseOrders(req,res)
+{
+    pool.query("UPDATE purchased_orders SET users_id = $1, manufacturers_id = $2, date_ordered = $3, date_received = $4 WHERE id = $5",[req.body.usersId, req.body.manufacturersId,req.body.dateOrdered, req.body.dateReceived, req.params.id], (error,results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(202).send();
+        }
+    }) 
+}
+
+function updateSaleOrders(req,res)
+{
+    pool.query("UPDATE sale_orders SET users_id = $1, customers_id = $2, date_ordered = $3, date_received = $4 WHERE id = $5",[req.body.usersId, req.body.customersId,req.body.dateOrdered, req.body.dateReceived, req.params.id], (error,results) =>
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        else{
+            res.status(202).send();
+        }
+    }) 
+}
+
+
 
 exports.getUsers = getUsers;
+exports.getManufacturers = getManufacturers;
+exports.getCustomers = getCustomers;
+exports.getItems = getItems;
+exports.getPurchaseOrders = getPurchaseOrders;
+exports.getSalesOrders = getSalesOrders;
+exports.getManufacturerById = getManufacturerById;
+exports.getUsersById = getUsersById;
+exports.getCustomerById = getCustomerById;
+exports.getPurchaseOrderById= getPurchaseOrderById;
+exports.getSaleOrdersById = getSaleOrdersById;
+exports.createNewUser = createNewUser;
+exports.createNewCustomer =createNewCustomer;
+exports.createNewItems = createNewItems;
+exports.createNewManufacturer = createNewManufacturer;
+exports.createNewSaleOrder = createNewSaleOrder;
+exports.createNewPurchaseOrder = createNewPurchaseOrder;
+exports.deleteCustomers = deleteCustomers;
+exports.deleteItems = deleteItems;
+exports.deleteManufacturer = deleteManufacturer;
+exports.deletePurchasedOrders = deletePurchasedOrders;
+exports.deleteSaleOrders = deleteSaleOrders;
+exports.deleteUser = deleteUser;
+exports.getItemById = getItemById;
+exports.updateUser = updateUser;
+exports.updateCustomer = updateCustomer;
+exports.updateItems = updateItems;
+exports.updateManufacturer =updateManufacturer;
+exports.updatePurchaseOrders = updatePurchaseOrders;
+exports.updateSaleOrders = updateSaleOrders;
